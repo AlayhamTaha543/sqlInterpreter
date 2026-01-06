c4eb2db2f232e3a07b595f4605ee2f5415f5f065
AlayhamTaha543 <alayham543279@gmail.com>
2026-01-06 17:25:22 +0300

    finish

--- antlr4/.antlr/SqlLexer.java
+++ antlr4/.antlr/SqlLexer.java
@@ -1,4 +1,4 @@
-// Generated from c:/Users/YaraALhasan/Desktop/sqlInterpreter/antlr4/SqlLexer.g4 by ANTLR 4.13.1
+// Generated from c:/Users/YaraALhasan/Downloads/Telegram Desktop/sqlInterpreter/sqlInterpreter/antlr4/SqlLexer.g4 by ANTLR 4.13.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
